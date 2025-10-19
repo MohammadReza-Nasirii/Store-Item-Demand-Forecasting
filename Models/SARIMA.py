271f125d75001382cb9f32e8d73e34b5b12af5a5
rez <zero.workspace@gmail.com>
2025-10-19 13:36:06 +0330

    SARIMS:adding lod and diffs

--- Models/SARIMA.py
+++ Models/SARIMA.py
@@ -10,4 +10,13 @@ test.set_index('date', inplace=True)
 
 test = test.sort_index()
 
-print(test.head())
+test['sales_log'] = np.log1p(test['sales'])
+
+test['sales_diff'] = test['sales_log'].diff()
+
+test['sales_diff_seasonal'] = test['sales_log'].diff(7)
+
+test = test.dropna()
+
+print(test[['sales', 'sales_log', 'sales_diff', 'sales_diff_seasonal']].head(10))
+
